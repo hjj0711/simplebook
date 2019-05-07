25c419025bb38e9a7c620b61ea88f997dbe8317c
hejj <735488739@qq.com>
2019-05-07 17:49:41 +0800

    header

--- src/common/header/index.js
+++ src/common/header/index.js
@@ -92,11 +92,6 @@ const mapDispathToProps = (dispatch) => {
     handleMouseEnter () { // 鼠标移入触发action
       dispatch(actionCreators.mouseEnter())
     },
-      dispatch(actionCreators.searchFocus())
-    },
-    handleMouseEnter () { // 鼠标移入触发action
-      dispatch(actionCreators.mouseEnter())
-    },
     handleMouseLeave () {
       dispatch(actionCreators.mouseLeave())
     },
